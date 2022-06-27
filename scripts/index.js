421436f014e4e1881a0dd49ed9fca381e60ae536
Olga Bobrova <eve982@yandex.ru>
2022-06-27 12:21:05 +0300

    6th project for 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ const settings = {
     submitButtonSelector: '.popup__submit-button',
     inactiveButtonClass: 'popup__submit-button_disabled',
     inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__input_error-message',
+    errorClass: 'popup__input-error-span_type_error-message',
   };  
 /**--------------------------------------------------------------------------------------- */
 /** Кнопка редактирования профиля. */
